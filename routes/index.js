e62cca85996af1842858c6ab56447642cc0eac82
nemimccarter <nemimccarter@gmail.com>
2018-03-17 21:51:40 -0700

    Log visitor data to cookie

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,31 @@ var uuidv4 = require('uuid/v4');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var visitorCookie = req.cookies.visitor.id;
+  var visitorCookie = req.cookies.visitorCookie;
+  var currentTime = new Date().toISOString();
 
   if (!visitorCookie) {
-    visitorCookie = 0;
-    res.cookie('visitor.id', visitorCookie);
+    console.log("Entered");
+    visitorCookie = {
+      'id' : process.env.VISITOR_COUNT,
+      'createdAt' : currentTime,
+      'firstTimeUser' : 'True',
+      'visitCount' : '1',
+      'numPagesVisited' : '1',
+      'layoutType' : chooseLayout(process.env.VISITOR_COUNT)
+    } 
+
     process.env.VISITOR_COUNT++;
-  }
+  } else {
+    visitorCookie.firstTimeUser = 'False';
+    visitorCookie.visitCount++;
+  };
+  console.log(visitorCookie);
 
-  console.log("visitCount: " + visitorCookie);
+  var index = chooseLayout(visitorCookie.id);
 
-  var index = chooseLayout(visitorCookie);
+  res.cookie('visitorCookie', visitorCookie);
 
-  console.log("welcome visitor " + visitorCookie);
-  console.log("type " + index );
-  res.cookie('visitCount', visitorCookie);
   res.render(index, { title: 'Express' });
 });
 
